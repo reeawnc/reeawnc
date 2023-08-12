b8af1591a920065d88321de476385cbea1618481
Rioghan <cunniffe.rioghan@gmail.com>
2023-08-12 19:52:25 +0100

    cleaning

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {"5257790_ball_game_sports_squash_icon.png": "31434c065262657b
 "icons/Icon-512.png": "31434c065262657b0945775b29a79299",
 "icons/Icon-maskable-192.png": "f7c6e7719b74e4eeeaab64a5b68513f3",
 "icons/Icon-maskable-512.png": "31434c065262657b0945775b29a79299",
-"index.html": "d81a479c45f0e186de2b880cf1348436",
-"/": "d81a479c45f0e186de2b880cf1348436",
-"main.dart.js": "f4d6fee410a8b3d9471605113fa14442",
+"index.html": "c47c5ce6bd9586ccfd301c53d0212fd2",
+"/": "c47c5ce6bd9586ccfd301c53d0212fd2",
+"main.dart.js": "a3de32c473a3edb58165d48a8669485b",
 "manifest.json": "aeb757867366dcf06f03be3994748d3f",
 "version.json": "bcf23df920c9c0398e0a1b75dbe0895c"};
 // The application shell files that are downloaded before a service worker can
